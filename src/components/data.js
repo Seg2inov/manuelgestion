30af66f4e219d17db2114b7c479c1f86feab75a4
heloise <heloise.pingitore@mmibordeaux.com>
2025-06-04 13:36:59 +0200

    barre de recherche part 1

--- src/components/data.js
+++ src/components/data.js
@@ -5,5 +5,102 @@ export default  {
         description : "Ici on met la description",
         theme : 'exemple de titre EXEMPLE DE TITRE '
     },
+    {
+        title : "Mise à jour du clavier caisse/balance",
+        link : "/docs/claviers/maj-clavier-caisse-balance",
+        description : "Vous pouvez personnaliser le clavier caisse/balance via cette interface.",
+        theme : 'mise a jour Mise à Jour Vous pouvez créer et personnaliser divers modèles de clavier. 	Supprimer la fonctionnalité de la touche. Valider la modification valider Pour créer une touche, cliquez sur un emplacement vide. Une fenêtre apparait, cliquez sur l’action que vous souhaitez ajouter à la touche. Vous pouvez modifier les touches des 3 claviers. '
+    },
+    {
+        title : "Mise à jour du clavier libre",
+        link : "/docs/claviers/maj-clavier-libre",
+        description : "Cette fenêtre vous permet de modifier les produits du clavier « Produits Divers » que vous avez en caisse (selon paramétrage). Ce clavier permet l’accès direct à 20 produits (les prix s’affichent).",
+        theme : 'mise a jour Mise à Jour clavier libre CLAVIER LIBRE Vous pouvez En caisse, le clavier libre se présente de la manière suivante, avec les prix affichés : Pour créer une nouvelle touche, cliquez sur un emplacement libre, puis sur NOUVEAU.'
+    },
+    {
+        title : "Mise à jour du clavier sans CAB",
+        link : "/docs/claviers/maj-clavier-sans-cab",
+        description : "Cette fenêtre vous permet de modifier les produits du clavier « Produits Divers » que vous avez en caisse (selon paramétrage). Ce clavier permet l’accès direct à 20 produits (les prix s’affichent).",
+        theme : 'mise a jour Mise à Jour clavier libre cab En caisse, le clavier des produits sans CAB se présente de la manière suivante, les prix n’étant pas affichés : Cependant, cette modification n’est pas reprise en caisse, seul le nom du produit tel qu’il est renseigné dans la fiche produit s’affiche. Il n’est donc pas utile de modifier le libellé lors de l’ajout d’une touche.'
+    },
+    {
+        title : "Mise à jour des clients",
+        link : "/docs/clients/maj-clients",
+        description : "Cette fenêtre vous permet de gérer vos clients.",
+        theme : "mise a jour Mise à Jour Informations fidélité  information fidelite En compte en compte historique HISTORIQUE Historique Badge client badge client Coupons coupons coupon Conpon NPAI npai Prix spécifiques prix specifiques Chiffre d'affaires du client chiffre d'affaire  Vous pouvez rechercher un client en saisissant son nom, son code client ou son code postal de résidence."
+    },
+    {
+        title : "Relevé des clients",
+        link : "/docs/clients/releve-clients",
+        description : "Ce module vous permet d’imprimer un relevé des ventes pour un client sélectionné sur une période définie.",
+        theme : "releve des clients releve client Releve clients   d’exporter générer le relevé client pour les clients sélectionnés sur la période définie. masquer les clients dont le solde est nul. Le relevé client reprend chaque transaction détaillée avec son moyen de paiement. la base client dans un fichierfacture récapitulative releve du client Il vous permet aussi de visualiser et d’imprimer la liste de vos clients avec leur solde de compte et le montant de crédit autorisé.."
+    },
+    {
+        title : "Les commandes centrales",
+        link : "/docs/commandes/commandes-centrales",
+        description : "L’expression de besoin permet au magasin d’envoyer au siège une demande d’approvisionnement de marchandise.",
+        theme : "les commandes centrales la commande centrale Expression par numéro : - Expression de besoin standard : Etablir une commande fournisseur pour le compte du magasin, ou de façon centralisée pour l’ensemble ou une partie des magasins ; l’expression de besoin Selon le paramétrage, l’accès à l’interface d’expression de besoin diffère : "
+    },
+    {
+        title : "Les commandes client",
+        link : "/docs/commandes/commandes-clients",
+        description : "Cette fenêtre vous permet de gérer vos commandes client.",
+        theme : "Les commandes peuvent être créées depuis l’interface caisse ou balance et ensuite être préparées ou complétées sur la partie gestion. Créer une nouvelle commande client creer une nouvelle Saisir et/ou préparer une commande client saisir Lorsque vous enregistrez une préparation (ou directement une commande, selon le paramétrage), caisse/balance via le bouton « Reprise Commande » dessus et ne peut pas encore être reprise dans l’interface caisse/balance. ensuite être préparées ou complétées sur la partie gestion. "
+    },
+    {
+        title : "Mise à jour des retraits de commandes",
+        link : "/docs/commandes/maj-retrait-commandes",
+        description : "Cette fenêtre vous permet de modifier la liste des modes de retrait des commandes.",
+        theme : "mise a jour Mise à Jour commande retrait "
+    },
+    {
+        title : "Mise à jour des unités de commandes",
+        link : "/docs/commandes/maj-unites-commandes",
+        description : "Cette fonctionnalité vous permet de renseigner des unités de commande, que vous pourrez choisir lors de la saisie d’une commande. ",
+        theme : "mise a jour Mise à Jour unites unite  de commande Ainsi, lors de la saisie d’une commande, vous pourrez retrouver cette liste dans la colonne COLISAGE. n’a pas d’incidence sur les stocks."
+    },
+    {
+        title : "Init. étiquettes électroniques",
+        link : "/docs/etiquettes/etiquettes-electroniques",
+        description : "Si votre magasin est équipé d’étiquettes électroniques, vous pouvez initialiser ces étiquettes en appuyant sur INIT. ETIQUETTE ELECT. Les prix seront alors mis à jour sur vos étiquettes.",
+        theme : "init etiquette electroniue etiquettes electroniques"
+    },
+     {
+        title : "Etiquettes produits",
+        link : "/docs/etiquettes/etiquettes-produits",
+        description : "Pour chaque réception validée, S2Cash enregistre automatiquement un nombre équivalent d’étiquettes pour les produits dont le paramètre « étiquette » est activé dans la fiche produit.",
+        theme : "etiquette Etiquettes produits Dans le cas de modification du prix de vente ou de création de produit, S2Cash enregistre automatiquement une étiquette. Permet de supprimer un produit de la liste. Astuce : pour supprimer tous les produits de la liste, utilisez le raccourci clavier windows CTRL+A et cliquez sur supprimer. 	Remplir le tableau des étiquettes en récupérant les produits et les quantités depuis un terminal de saisie (si paramétrée). 	Accéder directement à la fiche du produit sélectionné en mode modification. 	Ajouter un produit à la liste. Vous pouvez également ajouter un produit en saisissant directement son code dans la colonne CODE. Lorsque vous cliquez sur ETIQUETTES, vous pouvez choisir la taille souhaitée."
+    },
+    {
+        title : "Achat fournisseur",
+        link : "/docs/gestion-fournisseurs/achat-fournisseur",
+        description : "S2Cash vous permet de saisir, éditer et envoyer vos commandes fournisseurs.",
+        theme : "achat fournisseur ACHAT FOURNISSEUR Il vous propose également une aide à la décision par l’affichage de l’historique des derniers mouvements et, plus largement, l’historique sur une période de référence antérieure Sur la page d'accueil s'affiche la liste des commandes en cours, qui n'ont pas encore été validées. Suivre les prévisions d’épuisement des stocks en fonction du stock, des dernières ventes et des ventes sur une période précédente, et d’établir des propositions de commande par fournisseur. 	Générer une proposition de commande en fonction des mini et maxi renseignés sur les fiches produit. Créer une nouvelle commande à partir d’un catalogue fournisseur. Créer une nouvelle commande vierge 	Supprimer une commande en cours de saisie. Ajouter un produit à la commande. Vous pouvez également ajouter un produit en saisissant directement son code dans la colonne CODE. 	Ouvrir la fiche du produit sélectionné en mode modification. Le lancement de l’alerte de stocks permet la génération des commandes pour les produits arrivés en limite de stocks et qui vont tomber en rupture. S2Cash calcule la quantité moyenne de ventes par jour en se référant aux ventes des 4 dernières semaines et des 6 semaines de l’année précédente par rapport à la date du jour. n fonction du jour de commande fournisseur et de son délai moyen de livraison, S2Cash calcule le besoin en stocks pour chacun des produits. Lors de la validation de la commande, un mail est envoyé avec cette dernière si l’information de correspondance est enregistrée sur la fiche Fournisseur."
+    },
+    {
+        title : "Livraison fournisseur",
+        link : "/docs/gestion-fournisseurs/livraison-fournisseur",
+        description : "Cette interface permet de gérer et valider les réceptions de marchandises et de mettre à jour les stocks des produits.",
+        theme : "Lorsque vous validez une commande, elle est automatiquement basculée dans les attentes de réception. Vous pouvez également créer une nouvelle réception directement ici, sans passer par l’interface de commande fournisseur. Au lancement de la fenêtre, la liste des réceptions en attente apparait à l’écran cr éer une nouvelle réception sans passer par l'interface de commande 	Enregistrer la réception sans la valider, et ainsi pouvoir revenir dessus à tout moment. 	Insérer une ligne vide dans la réception. 	Ajouter un produit à la réception. Vous pouvez également ajouter un produit en saisissant directement son code dans la colonne CODE. fiche produit"
+    },
+    {
+        title : "Mise à jour des fournisseurs",
+        link : "/docs/gestion-fournisseurs/maj-fournisseurs",
+        description : "Cette fenêtre vous permet de gérer vos fournisseurs.",
+        theme : "mise a jour Mise à Jour Vous pouvez faire une recherche par nom, par code fournisseur ou code postal. Il est important de renseigner correctement le délai fournisseur ainsi que les jours de commande, ce qui va optimiser au maximum les stocks et les commandes de réapprovisionnement au plus juste. La gestion des marques permet de commander les produits aux fournisseurs par groupe de produits. "
+    },
+    {
+        title : "Ajustement de stocks",
+        link : "/docs/gestion-stocks/ajustement-stocks",
+        description : "Cette interface permet de créer le transfert des produits vers ou depuis un autre magasin.",
+        theme : "Elle peut être utilisée pour ajuster le stock, saisir de la casse ou encore gérer la consommation interne. Cette liste est paramétrable via le menu MAJ DES MOUVEMENTS (insérer un lien vers MAJ des mouvements). Le tableau suivant apparait alors à l’ecran, vous pouvez commencer à saisir le détail de votre ajustement. Saisir un ajustement de stocks Enregistrer l’ajustement sans le valider, et ainsi pouvoir revenir dessus à tout moment. 	Ajouter un produit. Vous pouvez également ajouter un produit en saisissant directement son code dans la colonne CODE. "
+    },
+    {
+        title : "Inventaire des stocks",
+        link : "/docs/gestion-stocks/inventaire-stocks",
+        description : "Cette option permet d’effectuer vos inventaires partiels ou en totalité.",
+        theme : "Lorsque vous créez un inventaire, un gel du stock est effectué (image du stock à l’instant T) et permettra une mise à jour en décalé des stocks lors de la validation. Dès lors que la comptabilisation des produits est faite, les ventes peuvent reprendre. Créer l'inventaire créer l'inventaire  	Générer un nouvel inventaire avec la totalité des produits. Générer un nouvel inventaire avec les produits d’un fournisseur. 	Modifier un inventaire en cours. Une fois dans l’inventaire, il vous faut renseigner la colonne stock réel correspondant à votre comptage. Selon le paramétrage, la colonne PUMP peut être modifiée. Pour rappel, le PUMP est le Prix Unitaire Moyen Pondéré, aussi appelé parfois CUMP pour Coût Unitaire Moyen Pondéré. Cette valeur correspond à la moyenne des prix d’achats d’un produit, pour peu que les réceptions soient toutes saisies et validées. Il est possible d’imprimer un état de saisie préparatoire pour écrire les quantités par produit. A la validation, un état de la saisie et des écarts est généré. Les stocks sont mis à jour par différence et enregistrés dans vos historiques."
+    },
+    
     ]
 }
